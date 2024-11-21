693dce910cc6e1eb2d4c6763e5fec46d9bb1aeba
rugs07 <rugwed07@gmail.com>
2024-11-21 13:24:48 +0530

    bot changes

--- bot.js
+++ bot.js
@@ -4,8 +4,6 @@ const express = require('express');
 const token = '7112412287:AAF0C35bDN_qj1tWD9GFATfpwQgZ_VG1eYA'; 
 const bot = new TelegramBot(token, { polling: true });
 
-console.log(bot, 'bot')
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,16 +11,47 @@ app.use(express.static('build'));
 
 const webAppUrl = 'https://tiger-coin.x1cryptoscripts.com/';
 
+
+const customKeyboard = {
+  reply_markup: {
+    keyboard: [
+      [{ text: 'Open Tiger Coin' }], 
+      [{ text: '/help' }] 
+    ],
+    resize_keyboard: true, 
+    one_time_keyboard: true, 
+  },
+};
+
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Welcome! Click below to visit our site:', {
+    bot.sendMessage(chatId, 'Welcome to Tiger Coin Bot! Click the button below to open the site or type /help for more info.', customKeyboard);
+});
+
+bot.onText(/\/help/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Here is a list of available commands:\n\n/start - Start the bot\n/help - Get help with commands\n\nClick "Open Tiger Coin" to visit our website.', {
         reply_markup: {
             inline_keyboard: [[{
                 text: 'Open Tiger Coin',
-                web_app: { url: webAppUrl } 
-            }]]
-        }
+                web_app: { url: webAppUrl },
+            }]],
+        },
     });
+});
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    if (msg.text === 'Open Tiger Coin') {
+        bot.sendMessage(chatId, 'Click below to open the site', {
+            reply_markup: {
+                inline_keyboard: [[{
+                    text: 'Open Tiger Coin',
+                    web_app: { url: webAppUrl },
+                }]],
+            },
+        });
+    }
 });
 
 app.listen(PORT, () => {
